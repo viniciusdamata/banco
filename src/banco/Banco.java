674729c64ca8883ee35d25bbda26d6ab2f3ad1e6
Vinicius Lima <vinilima98.vl@gmail.com>
2018-06-14 17:01:51 -0400

    commit dia 14/06 continuar

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -11,16 +11,7 @@ public class Banco {
         agenciaVet = new agencia[x];
         clienteVet = new cliente[y];
         contaVet = new conta[z];
-       /* for (int i = 0; i < agenciaVet.length; i++) {;
-            agenciaVet[i] = new agencia();
-        }
-        for (int i = 0; i < clienteVet.length; i++) {
-            clienteVet[i] = new cliente();
-        }
-        for (int i = 0; i < contaVet.length; i++) {
-            contaVet[i] = new conta();
-            
-        }*/
+    
     }
     public void cadastrarAgencia(){
         for (int i = 0; i < agenciaVet.length; i++) {
@@ -39,11 +30,43 @@ public class Banco {
             clienteVet[i].setCpfIndicou();
         }
     }
-    
-    public void cadastrarConta(int numAgencia, int conta, int cpf){
+    public boolean verificaAgencia(int numAgencia, int i){
+        if(i == 0 && numAgencia != agenciaVet[i].getNumero()){
+          return false;
+        }
+        else if(numAgencia == agenciaVet[i].getNumero()){
+          return true;   
+        }
+            return verificaAgencia(numAgencia, i-1);
+    }
+    public boolean verificaCliente(int cpf, int i){
+        if(i == 0 && cpf != clienteVet[i].getCpf()){
+            return false;
+        }
+        else if(cpf == clienteVet[i].getCpf()){
+            return true;
+        }
+            return verificaCliente(cpf, i-1);
+    }
+    public void verificarDados(int numAgencia, int conta, int cpf){
+            if(verificaAgencia(numAgencia, agenciaVet.length-1)){
+                if(verificaCliente(cpf, agenciaVet.length-1)){
+                    cadastraConta(numAgencia, conta, cpf);
+                }
+                else{
+                    System.out.println("Cliente nao encontrado! tente novamente");
+                }
+            }
+            else{
+                System.out.println("Agencia nao encontrada! tente novamente");
+            }
+    }
+    public void cadastraConta(int numAgencia, int conta, int cpf){
+        /*Continuar daqui*/
+        for (int i = 0; i < contaVet.length; i++) {
             
-        
-        
+            
+        }
     }
     public static void main(String[] args) {
         Banco banco = new Banco(Input.readInt("Digite o numero de clientes: "),
